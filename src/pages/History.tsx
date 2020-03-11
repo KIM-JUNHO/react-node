74454ec863684a1e3788eb27bcc3b7143f5714c5
prpr48 <prpr48@naver.com>
2020-03-11 22:23:42 +0900

    useQuery

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,5 +1,40 @@
 import * as React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_RULES = gql`
+  {
+    Rules {
+      id
+      srcAddr
+      dstAddr
+    }
+  }
+`;
 
 export default function History() {
-  return <div>History</div>;
+  const { loading, error, data } = useQuery(GET_RULES);
+  if (loading) {
+    return 'loading...';
+  }
+  if (data && data.Rules) {
+    return (
+      <table>
+        <thead>
+          <tr>
+          <th>#</th>
+          <th>Src IP</th>
+          <th>Dst IP</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.Rules.map(rule => 
+          <tr>
+            <td>{rule.id}</td>
+            <td>{rule.srcAddr}</td>
+            <td>{rule.dstAddr}</td>
+          </tr>)}
+        </tbody>
+      </table>
+  }
 }
